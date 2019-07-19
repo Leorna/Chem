6def14b18875bd814ccac5c234f995393df32bc9
Leorna <gabrielcapuerj@gmail.com>
2019-07-19 20:45:15 -0300

    files

--- main.py
+++
@@ -1,6 +0,0 @@
-from window import *
-
-
-if __name__ == '__main__':
-    chemwin = ChemWindow()
-    chemwin.show_chem_window()
--- winconfig.py
+++
@@ -1,5 +0,0 @@
-class Window:
-    """A class with the configurations of the window"""
-    def __init__(self):
-        self.width = 700
-        self.height = 500
